630129c9b70f10057ea184128a3178faf6f0f935
ceddah <cedanik1@gmail.com>
2023-02-05 11:04:43 +0100

    t

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -1,42 +1,44 @@
 class APIFeatures {
-    constructor(query, queryStr) {
-        this.query = query;
-        this.queryStr = queryStr;
-    }
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
 
-    search() {
-        const keyword = this.queryStr.keyword ? {
-            name: { 
-                $regex: this.queryStr.keyword,
-                $options: 'i'
-            }
-        } : {};
+  search() {
+    const keyword = this.queryStr.keyword
+      ? {
+          name: {
+            $regex: this.queryStr.keyword,
+            $options: "i",
+          },
+        }
+      : {};
 
-        this.query = this.query.find({ ...keyword });
-        return this;
-    }
+    this.query = this.query.clone().find({ ...keyword });
+    return this;
+  }
 
-    filter() {
-        const queryCopy = { ...this.queryStr };
-        //Removing keywords that we will use in other features
-        const removeFields = ['keyword', 'limit', 'page']
-        removeFields.forEach(el => delete queryCopy[el]);
+  filter() {
+    const queryCopy = { ...this.queryStr };
+    //Removing keywords that we will use in other features
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((el) => delete queryCopy[el]);
 
-        //Advance Filter Options - for price, ratings...
-        let queryStr = JSON.stringify(queryCopy)
-        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`);
-        
-        this.query = this.query.find(JSON.parse(queryStr));
-        return this;
-    }
+    //Advance Filter Options - for price, ratings...
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
 
-    pagination(resPerPage) {
-        const currentPage = Number(this.queryStr.page) || 1;
-        const skip = resPerPage * (currentPage - 1);
+    this.query = this.query.clone().find(JSON.parse(queryStr));
+    return this;
+  }
 
-        this.query = this.query.limit(resPerPage).skip(skip);
-        return this;
-    }
+  pagination(resPerPage) {
+    const currentPage = Number(this.queryStr.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+
+    this.query = this.query.limit(resPerPage).skip(skip);
+    return this;
+  }
 }
 
 module.exports = APIFeatures;
