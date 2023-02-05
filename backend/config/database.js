2bc7ca14475efa29d7f5e5e44a1b0206c77a0b93
ceddah <cedanik1@gmail.com>
2023-02-05 09:38:00 +0100

    deployment fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    }).then(() => {
-        console.log('MongoDB Database connected.');
+  mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
+    .then(() => {
+      console.log("MongoDB Database connected.");
+    });
 };
 
 module.exports = connectDatabase;
